294c26eccdb520bbc37a460471215aa1ad5cdc30
André Wagner <andre.nho@gmail.com>
2023-04-27 13:08:37 -0500

    .

--- vm/main.c
+++ vm/main.c
@@ -1,17 +1,14 @@
-#include <vector>
-#include <cstdio>
+#include <stdio.h>
 #ifdef PICO_DEFAULT_BOARD
 #  include "pico/stdlib.h"
 #endif
 
 int main() {
-    std::vector<int> v { 1, 2, 3 };
-
 #ifdef PICO_DEFAULT_BOARD
     stdio_init_all();
 #endif
-    while (true) {
-        printf("Hello world %d!\n", v.at(2));
+    while (1) {
+        printf("Hello world %d!\n", 3);
 #ifdef PICO_DEFAULT_BOARD
         sleep_ms(1000);
 #endif
